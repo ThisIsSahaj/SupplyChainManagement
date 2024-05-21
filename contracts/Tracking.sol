fc8b4eceeddffcc3c37d68090bc10e15bffaf2ec
Mahendra Kumawat <117855423+mk016@users.noreply.github.com>
2024-05-21 17:41:12 +0000

    Tracking sol

--- contracts/Tracking.sol
+++ contracts/Tracking.sol
@@ -3,5 +3,15 @@
 pragma solidity 0.8.0;
 
 contract Tracking {
-    enum ShipmentStatus { }
+    enum ShipmentStatus { PENDING, IN_TRANSIT , DELIVERED }
+
+    struct Shipment {
+        address sender;
+        address receiver;
+        uint256 pickupTime;
+        uint256 distance;
+        uint256 price;
+        ShipmentStatus status;
+        
+    }
 }
