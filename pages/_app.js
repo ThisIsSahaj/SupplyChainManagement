07f765508a05bee2b1d503f046b60bc38d3b555b
Mahendra Kumawat <117855423+mk016@users.noreply.github.com>
2024-05-24 18:05:21 +0000

    Mahendra

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
 import "../styles/globals.css";
 
+
+//INTERNAL IMPORT 
+
+import { TrackingProvider } from "../Conetxt/Tracking";
+
+import {Navbar ,Footer} from "../Components";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+  <>
+    <TrackingProvider>
+    <Navbar />
+     <Component {...pageProps} />;
+    </TrackingProvider>
+    <Footer />
+ </>
+  );
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,13 @@
 const hre = require("hardhat");
 
 async function main() {
-    const Lock = await hre.ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime ,{ value : lockedAmount});
-
-    await lock.deployed();
-
-    console     
+    const Tracking = await hre.ethers.getContractFactory("Tracking");
+    const tracking = await Tracking.deploy(); // Added 'await' and corrected syntax
+    await tracking.deployed();
+    console.log(`Tracking deployed to ${tracking.address}`);
 }
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
