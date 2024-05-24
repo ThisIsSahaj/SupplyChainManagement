8ae098b4990c92718d7eb6875c41dca0faf223f5
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-05-24 15:53:43 +0530

    index.js

---
+++ Components/index.js
@@ -0,0 +1,21 @@
+import React ,{ useState, useEffect, useCallback} from "react";
+
+//internal import
+import{
+    Table,
+    Form,
+    Services,
+    Profile,
+    CompleteShipment,
+    GetShipment,
+    StartShipment,
+} from "../Components/index.js";
+import {TrackingContext} from "../Conetxt/TrackingContext";
+
+const index=() =>{
+    const{
+        currentUser,
+        createShipment,
+
+    }
+}
