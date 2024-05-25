a0802a86d70f8f1fb1d8b17ae2dca38fbcef88c2
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-05-25 23:13:39 +0530

    ui-changes

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -29,11 +29,11 @@ export default ()=>{
           <div className="space-y-6">
             <img src="logo.png" className="w-32"/>
           <p className="max-w-md">
-            devloped by Pull Stackers.
+            {/* devloped by Pull Stackers. */}
           </p>
           <ul className="flex flex-wrap gap-4 text-sm sm:text-base">
             {footerNavs.map((item,idx)=>(
-              <li className="text-gray-800 hover:text-gray-500 duration-150">
+              <li className="text-gray-800 hover:text-blue-500 duration-150">
                 <a key={idx} href={item.href}>
                   {item.name}
                 </a>
@@ -42,13 +42,13 @@ export default ()=>{
           </ul>
           </div>
             <div className="mt-6">
-              <p className="text-gray-700 font-semibold">Get the app</p>
+              <p className="text-gray-700 font-semibold"></p>
               <div className=" flex items-center gap-3 sm:block">
                 <a href="javasvript:void(0)">
-                <Fot1/>
+                
                 </a>
                 <a href="javasvript:void(0)" className="mt-0 block sm:mt-3">
-                <Fot2/>
+                
                 </a>
                </div>
               </div>
